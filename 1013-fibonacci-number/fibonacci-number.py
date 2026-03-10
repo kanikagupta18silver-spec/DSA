2ff015421c15df2e1528eba8f8bbdca211cb91ac
kanikagupta18silver-spec <kanikagupta18silver@gmail.com>
2026-03-10 13:30:40 +0530

    Time: 43 ms (79.03%) | Memory: 19.1 MB (97.44%) - LeetSync

---
+++ 1013-fibonacci-number/fibonacci-number.py
@@ -0,0 +1,12 @@
+class Solution:
+    def fib(self, n: int) -> int:
+        memo = {0:0, 1:1}
+        
+        def f(n):
+            if n in memo: return memo[n]
+            result = f(n-1) + f(n-2)
+            memo[n] = result
+            return result
+
+        return f(n)
+        
