33765d75ff34d068322a98d83d2d6eb9248ec04e
kanikagupta18silver-spec <kanikagupta18silver@gmail.com>
2026-03-10 13:24:16 +0530

    Time: 0 ms (100.00%) | Memory: 47.1 MB (95.42%) - LeetSync

---
+++ 268-missing-number/missing-number.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+       int n =  nums.length;
+       int sum=0;
+       for(int i=0;i<n;i++)
+       {
+       sum = sum + nums[i];
+       }
+
+       int actualsum = (n * (n + 1 )) / 2;
+    
+    return actualsum - sum ;
+ }
+}
